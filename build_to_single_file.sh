92958541e7c3c96ef2aa9889b49cb9e5486b5581
William.Meaton <will.meaton@gmail.com>
2023-10-03 20:34:17 +0100

    Added support for macos in build steps

--- build_to_single_file.sh
+++ build_to_single_file.sh
@@ -1,3 +1,5 @@
 # .. lets hope this works :D
 dotnet publish -r win-x64 -c Release -p:PublishSingleFile=true -p:PublishReadyToRun=true --self-contained true -p:PublishTrimmed=true
 dotnet publish -r linux-x64 -c Release -p:PublishSingleFile=true -p:PublishReadyToRun=true --self-contained true -p:PublishTrimmed=true
+dotnet publish -r osx-arm64 -c Release -p:PublishSingleFile=true -p:PublishReadyToRun=true --self-contained true -p:PublishTrimmed=true
+dotnet publish -r osx-x64 -c Release -p:PublishSingleFile=true -p:PublishReadyToRun=true --self-contained true -p:PublishTrimmed=true
